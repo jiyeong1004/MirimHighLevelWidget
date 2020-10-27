22340006a7601372d60bee246f12ae71482540e4
jiyeong1004 <ak1157@naver.com>
2020-10-27 20:36:12 +0900

    Complete Setup RadioButton Event Programming

--- app/src/main/java/com/example/mirimhighlevelwidget/MainActivity.java
+++ app/src/main/java/com/example/mirimhighlevelwidget/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.mirimhighlevelwidget;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.RadioButton;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    RadioButton radioDate, radioTime;
+    CalendarView calendar1;
+    TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
+        radioDate = findViewById(R.id.radio_date);
+        radioTime = findViewById(R.id.radio_time);
+        radioDate.setOnClickListener(radioListener);
+        radioTime.setOnClickListener(radioListener);
+        calendar1 = findViewById(R.id.calender1);
+        timePicker = findViewById(R.id.time_pick);
     }
+
+    View.OnClickListener radioListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.radio_date:
+                    calendar1.setVisibility(View.VISIBLE);
+                    timePicker.setVisibility(View.INVISIBLE);
+                    break;
+                case R.id.radio_time:
+                    calendar1.setVisibility(View.INVISIBLE);
+                    timePicker.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+    };
 }
